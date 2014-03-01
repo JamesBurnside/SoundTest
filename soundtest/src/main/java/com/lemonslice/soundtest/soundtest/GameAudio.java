f9f9f148d25e3475ac01a876d91ed2dab1a6be81
James Burnside <mail@jamesburnside.com>
2014-03-01 15:19:44 +0000

    Loads of changes including separate doc

---
+++ soundtest/src/main/java/com/lemonslice/soundtest/soundtest/GameAudio.java
@@ -0,0 +1,85 @@
+package com.lemonslice.soundtest.soundtest;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+import com.musicg.wave.Wave;
+
+import java.io.InputStream;
+
+/**
+ * Created by Sam on 01/03/14.
+ */
+public class GameAudio {
+    static MediaPlayer mediaPlayerx = new MediaPlayer();
+    static double bpm = 104.993; //beat per minute
+    static double spb = 60.0/bpm; //seconds per beat
+    static int intro = 250;
+    static double mean = 0;
+
+    public static void startMedia()
+    {
+        mediaPlayerx.start();
+    }
+
+    public static void pauseMedia()
+    {
+        mediaPlayerx.pause();
+    }
+
+    public static void stopMedia(Context c)
+    {
+        mediaPlayerx.stop();
+        mediaPlayerx.release();
+        mediaPlayerx = MediaPlayer.create(c, R.raw.eiawav);
+    }
+
+    public static double plzGetBeatFraction()
+    {
+        int curPos = mediaPlayerx.getCurrentPosition()-intro ;
+        double frac = (curPos % spb*1000.0)/(spb*1000.0);
+        return frac;
+    }
+
+
+    public static int plzGetIntenseValue(Context c)
+    {
+
+        InputStream is = c.getResources().openRawResource(R.raw.eiawav);
+        Wave wave = new Wave(is);
+
+        short[] amps = wave.getSampleAmplitudes();
+
+
+        if(mean == 0)
+        {
+            mean = (amps[0] + amps[1])/2.0;
+            for(int i=2; i<amps.length; i++)
+            {
+                if (amps[i] < 0) amps[i] *= -1;
+                mean = (mean*(i) + amps[i]) / (i+1);
+            }
+        }
+
+
+
+
+        double upperThreshold = mean*2.0;
+        double lowerThreshold = mean/2.0;
+
+        double curPos = mediaPlayerx.getCurrentPosition()-intro;
+        double percent = curPos/(mediaPlayerx.getDuration()-intro);
+        double amplitude = amps[(int)(percent * amps.length)];
+
+
+        if(amplitude < lowerThreshold)
+            return 1;
+        else if(amplitude < mean)
+            return 2;
+        else if(amplitude < upperThreshold)
+            return 3;
+        else
+            return 4;
+    }
+
+}
--- soundtest/src/main/java/com/lemonslice/soundtest/soundtest/MainActivity.java
+++ soundtest/src/main/java/com/lemonslice/soundtest/soundtest/MainActivity.java
@@ -1,103 +1,58 @@
 package com.lemonslice.soundtest.soundtest;
 
 import android.app.Activity;
-import android.content.ContentResolver;
-import android.database.Cursor;
-import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    MediaPlayer mediaPlayerx = new MediaPlayer();
     Button btnStart;
     Button btnStop;
     Button btnFraction;
-    double bpm = 104.993; //beat per minute
-    double spb = 60.0/bpm; //seconds per beat
-    int intro = 250;
-
+    Button btnJames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mediaPlayerx = MediaPlayer.create(this, R.raw.pd);
         btnStart = (Button)findViewById(R.id.btnStartMusic);
         btnStop = (Button)findViewById(R.id.btnStopMusic);
+        btnJames = (Button)findViewById(R.id.btnJames);
         btnFraction = (Button) findViewById(R.id.btnFraction);
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try
-                {
-                   mediaPlayerx.start();
-
-                }
-                catch(Exception e)
-                {
-                    Toast.makeText(getApplicationContext(), "Error starting music", Toast.LENGTH_LONG).show();
-                }
+                GameAudio.startMedia();
             }
         });
 
         btnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mediaPlayerx.stop();
-                mediaPlayerx.release();
-                mediaPlayerx = MediaPlayer.create(MainActivity.this, R.raw.pd);
+                GameAudio.stopMedia(getApplicationContext());
             }
         });
 
         btnFraction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                plzGetFraction();
+                Double rval = GameAudio.plzGetBeatFraction();
+                Toast.makeText(getApplicationContext(), "Frac: "+rval, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnJames.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int rval = GameAudio.plzGetIntenseValue(getApplicationContext());
+                Toast.makeText(getApplicationContext(), "Intensity: "+rval, Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    public Double plzGetFraction()
-    {
-        int curPos = mediaPlayerx.getCurrentPosition()-intro ;
-        double frac = (curPos % spb*1000.0)/(spb*1000.0);
-
-        Toast.makeText(getApplicationContext(),
-                "CurPos: "+Integer.toString(curPos)+" frac: "+Double.toString(frac),
-                Toast.LENGTH_SHORT).show();
-
-        return frac;
-    }
 
 }
