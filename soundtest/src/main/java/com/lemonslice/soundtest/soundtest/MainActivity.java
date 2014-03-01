d7673efbc7b0ab57e536b89b3fa6e7b7f4f4b72b
James Burnside <mail@jamesburnside.com>
2014-03-01 10:24:27 +0000

    work

--- soundtest/src/main/java/com/lemonslice/soundtest/soundtest/MainActivity.java
+++ soundtest/src/main/java/com/lemonslice/soundtest/soundtest/MainActivity.java
@@ -1,8 +1,11 @@
 package com.lemonslice.soundtest.soundtest;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.database.Cursor;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.Menu;
@@ -16,6 +19,8 @@ public class MainActivity extends Activity {
     MediaPlayer mediaPlayer = new MediaPlayer();
     Button btnStart;
     Button btnStop;
+    ContentResolver contentResolver;
+    Uri muisicUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,26 +30,57 @@ public class MainActivity extends Activity {
         btnStart = (Button)findViewById(R.id.btnStartMusic);
         btnStop = (Button)findViewById(R.id.btnStopMusic);
 
+//        contentResolver = getContentResolver();
+//        muisicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+//        Cursor cursor = contentResolver.query(muisicUri, null, null, null, null);
+//
+//        if (cursor == null)
+//        {
+//            // query failed, handle error.
+//        }
+//        else if (!cursor.moveToFirst())
+//        {
+//            // no media on the device
+//        }
+//        else
+//        {
+//            int titleColumn = cursor.getColumnIndex(android.provider.MediaStore.Audio.Media.TITLE);
+//            int idColumn = cursor.getColumnIndex(android.provider.MediaStore.Audio.Media._ID);
+//            do {
+//                long thisId = cursor.getLong(idColumn);
+//                String thisTitle = cursor.getString(titleColumn);
+//                // ...process entry...
+//            } while (cursor.moveToNext());
+//        }
+
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         try
         {
             mediaPlayer.setDataSource(MediaStore.Audio.Media.DATA);
 
         } catch (Exception e) {
-            Toast.makeText(getApplicationContext(), "no music", 3);
+            Toast.makeText(this, "no music", Toast.LENGTH_LONG).show();
         }
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                try
+                {
+                    mediaPlayer.prepare(); // might take long! (for buffering, etc)
+                    mediaPlayer.start();
+                }
+                catch(Exception e)
+                {
+                    Toast.makeText(getApplicationContext(), "Error starting music", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
         btnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                mediaPlayer.stop();
             }
         });
     }
