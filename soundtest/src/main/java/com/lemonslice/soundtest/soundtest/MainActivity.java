160469d87414d9e364d8e59b4b21a937487e1785
James Burnside <mail@jamesburnside.com>
2014-03-01 15:42:46 +0000

    Intensities need work

--- soundtest/src/main/java/com/lemonslice/soundtest/soundtest/MainActivity.java
+++ soundtest/src/main/java/com/lemonslice/soundtest/soundtest/MainActivity.java
@@ -3,36 +3,30 @@ package com.lemonslice.soundtest.soundtest;
 import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 public class MainActivity extends Activity {
 
     Button btnStart;
     Button btnStop;
     Button btnFraction;
-<<<<<<< HEAD
     Button btnJames;
-=======
-    double bpm = 148.003; //beat per minute
-    double spb = 60.0/bpm; //seconds per beat
-    int intro = 1346;
+    Long mStartTime;
+    Handler mHandler;
 
->>>>>>> 6b9b13e6d39c46b4c1cf1dec8aa304307dfefb08
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-=======
-        mediaPlayerx = MediaPlayer.create(this, R.raw.eia);
-        int length = mediaPlayerx.getCurrentPosition();
-        Double bps = 60.0/bpm;
-
-        //Toast.makeText(getApplicationContext(), Integer.toString(intro), Toast.LENGTH_SHORT).show();
->>>>>>> 6b9b13e6d39c46b4c1cf1dec8aa304307dfefb08
         btnStart = (Button)findViewById(R.id.btnStartMusic);
         btnStop = (Button)findViewById(R.id.btnStopMusic);
         btnJames = (Button)findViewById(R.id.btnJames);
@@ -48,13 +42,7 @@ public class MainActivity extends Activity {
         btnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-<<<<<<< HEAD
                 GameAudio.stopMedia(getApplicationContext());
-=======
-                mediaPlayerx.stop();
-                mediaPlayerx.release();
-                mediaPlayerx = MediaPlayer.create(MainActivity.this, R.raw.eia);
->>>>>>> 6b9b13e6d39c46b4c1cf1dec8aa304307dfefb08
             }
         });
 
@@ -69,11 +57,10 @@ public class MainActivity extends Activity {
         btnJames.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int rval = GameAudio.plzGetIntenseValue(getApplicationContext());
-                Toast.makeText(getApplicationContext(), "Intensity: "+rval, Toast.LENGTH_SHORT).show();
+                int intensity = GameAudio.plzGetIntenseValue(getApplicationContext());
+                TextView tv = (TextView)findViewById(R.id.txtvIntensity);
+                tv.setText(String.valueOf(intensity));
             }
         });
     }
-
-
 }
