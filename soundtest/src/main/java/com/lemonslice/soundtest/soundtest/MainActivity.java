ea0738d02ba885652e965b8649a228b183a576c2
James Burnside <mail@jamesburnside.com>
2014-03-01 11:11:36 +0000

    Saving... Please do not turn off the power

--- soundtest/src/main/java/com/lemonslice/soundtest/soundtest/MainActivity.java
+++ soundtest/src/main/java/com/lemonslice/soundtest/soundtest/MainActivity.java
@@ -30,49 +30,52 @@ public class MainActivity extends Activity {
         btnStart = (Button)findViewById(R.id.btnStartMusic);
         btnStop = (Button)findViewById(R.id.btnStopMusic);
 
-//        contentResolver = getContentResolver();
-//        muisicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-//        Cursor cursor = contentResolver.query(muisicUri, null, null, null, null);
-//
-//        if (cursor == null)
-//        {
-//            // query failed, handle error.
-//        }
-//        else if (!cursor.moveToFirst())
-//        {
-//            // no media on the device
-//        }
-//        else
-//        {
-//            int titleColumn = cursor.getColumnIndex(android.provider.MediaStore.Audio.Media.TITLE);
-//            int idColumn = cursor.getColumnIndex(android.provider.MediaStore.Audio.Media._ID);
-//            do {
-//                long thisId = cursor.getLong(idColumn);
-//                String thisTitle = cursor.getString(titleColumn);
-//                // ...process entry...
-//            } while (cursor.moveToNext());
-//        }
 
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        try
-        {
-            mediaPlayer.setDataSource(MediaStore.Audio.Media.DATA);
-
-        } catch (Exception e) {
-            Toast.makeText(this, "no music", Toast.LENGTH_LONG).show();
-        }
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try
                 {
-                    mediaPlayer.prepare(); // might take long! (for buffering, etc)
-                    mediaPlayer.start();
+                    Cursor cursor = contentResolver.query(muisicUri, null, null, null, null);
+
+                    if (cursor == null)
+                    {
+                        // query failed, handle error.
+                    }
+                    else if (!cursor.moveToFirst())
+                    {
+                        // no media on the device
+                    }
+                    else
+                    {
+                        int titleColumn = cursor.getColumnIndex(android.provider.MediaStore.Audio.Media.TITLE);
+                        int idColumn = cursor.getColumnIndex(android.provider.MediaStore.Audio.Media._ID);
+                        do {
+                            mediaPlayer.reset();
+                            long thisId = cursor.getLong(idColumn);
+                            String thisTitle = cursor.getString(titleColumn);
+                            Toast.makeText(getApplicationContext(), "got: "+thisTitle, Toast.LENGTH_SHORT).show();
+                                try
+                                {
+                                    mediaPlayer.setDataSource(AudioUriPath);
+
+                                } catch (Exception e) {
+                                    Toast.makeText(getApplicationContext(), "no music: "+e+" "+thisTitle, Toast.LENGTH_LONG).show();
+                                }
+
+                            mediaPlayer.prepare();
+                            mediaPlayer.start();
+
+
+                        } while (cursor.moveToNext());
+                    }
+
+
                 }
                 catch(Exception e)
                 {
-                    Toast.makeText(getApplicationContext(), "Error starting music", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Error starting music: "+e, Toast.LENGTH_LONG).show();
                 }
             }
         });
